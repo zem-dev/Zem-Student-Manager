9b85c2fa7628609215e70db6ecc15fb6f1a68ff9
Ismael B <eggboissaye@gmail.com>
2021-08-19 18:44:17 -0400

    Co-authored-by: ZB0SS <ZB0SS@users.noreply.github.com> Co-authored-by: matees <Matees1@users.noreply.github.com>

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../styles/Header.scss'
+
+const Header = () => {
+    return (
+        <div>
+            <h1 className="title">Student Management</h1>
+            
+        </div>
+    )
+}
+
+export default Header
